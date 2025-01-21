4ebbc45e7ffa178a77c87063eda0fd4b018bfd9d
julian.lucks <julian.lucks@digital-at-work.de>
2025-01-21 11:05:35 +0100

    setup framerate

--- main.py
+++ main.py
@@ -9,14 +9,26 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Create a Clock instance
+    clock = pygame.time.Clock()
+    # Initialize delta time variable
+    dt = 0
+
     running = True
     while running:
+        # Event handling
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            color = (0,0,0)
-            screen.fill(color)
-            pygame.display.flip()
+        # Fill the screen with black
+        color = (0,0,0)
+        screen.fill(color)
+        # Update the display
+        pygame.display.flip()
+        # Cap the frame rate at 60 FPS and get delta time
+        framerate = 60
+        dt = clock.tick(framerate) / 1000 # Convert milliseconds to seconds
+        print(f"Delta time: {dt:.4f} seconds")
 
 if __name__ == "__main__":
     main()
