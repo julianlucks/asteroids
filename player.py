342c722d1d941cd507d1e7f27b6cf18c52a849d5
julian.lucks <julian.lucks@digital-at-work.de>
2025-01-25 15:52:12 +0100

    introduce rate limit

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 from circleshape import CircleShape 
 from shot import Shot
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
 import pygame
 
 class Player(CircleShape):
@@ -10,6 +10,7 @@ class Player(CircleShape):
         # Call the parent class's constructor
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0  # Timer to manage shooting cooldown
 
         # Add this instance to the assigned groups
         if Player.containers:
@@ -37,9 +38,14 @@ class Player(CircleShape):
         # rotate based on direction (-1 for left, 1 for right)
         self.rotation += direction * PLAYER_TURN_SPEED * dt
 
-    # function for the rotation key bindings
+    # update the player
     def update(self, dt):
         keys = pygame.key.get_pressed()
+
+        # Decrease the shoot timer by dt
+        if self.shoot_timer > 0:
+            self.shoot_timer -= dt
+
         if keys[pygame.K_a]:  # turn left
             self.rotate(dt, -1)  # Pass -1 for left
         if keys[pygame.K_d]:
@@ -48,7 +54,8 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(dt)
-        if keys[pygame.K_SPACE]:
+        # Shoot if spacebar is pressed and cooldown has expired
+        if keys[pygame.K_SPACE] and self.shoot_timer <= 0:
             self.shoot()
 
     def move(self, dt):
@@ -56,6 +63,8 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
     
     def shoot(self):
+        # Reset the shoot timer to the cooldown duration
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
         # Create a shot object at the player's position
         shot_velocity = pygame.Vector2(0, 1)  # Initial direction (upwards)
         shot_velocity = shot_velocity.rotate(self.rotation)  # Rotate to player's direction
